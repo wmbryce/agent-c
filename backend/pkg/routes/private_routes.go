d053b566fee16d6692a37134341e2f4cd2ada080
Michael Bryce <michaelbryce@me.com>
2025-11-30 21:09:28 -0700

    chore: init ai controller

--- backend/pkg/routes/private_routes.go
+++ backend/pkg/routes/private_routes.go
@@ -11,21 +11,17 @@ func PrivateRoutes(a *fiber.App) {
 	// Create routes group.
 	route := a.Group("/api/v1")
 
+	route.Get("/ai/list", middleware.JWTProtected(), controllers.GetAIList)   // get list of AI models
+
 	// Routes for POST method:
-	route.Post("/book", middleware.JWTProtected(), controllers.CreateBook)              // create a new book
-	route.Post("/user/sign/out", middleware.JWTProtected(), controllers.UserSignOut)    // de-authorization user
-	route.Post("/token/renew", middleware.JWTProtected(), controllers.RenewTokens)      // renew Access & Refresh tokens
-	route.Post("/openai/chat", middleware.JWTProtected(), controllers.ChatCompletion)   // send chat completion request to OpenAI
+	// route.Post("/openai/chat", middleware.JWTProtected(), controllers.ChatCompletion)   // send chat completion request to OpenAI
+	route.Post("/ai/chat", middleware.JWTProtected(), controllers.ChatCompletion)   // send chat completion request to OpenAI
 	
 	// Blockchain routes:
 	route.Post("/blockchain/balance", middleware.JWTProtected(), controllers.GetBalance)              // get ETH balance
-	route.Post("/blockchain/receipt", middleware.JWTProtected(), controllers.GetTransactionReceipt)   // get transaction receipt
-	route.Post("/blockchain/send-ether", middleware.JWTProtected(), controllers.SendEther)            // send ETH
+	// route.Post("/blockchain/receipt", middleware.JWTProtected(), controllers.GetTransactionReceipt)   // get transaction receipt
+	// route.Post("/blockchain/send-ether", middleware.JWTProtected(), controllers.SendEther)            // send ETH
 	route.Get("/blockchain/info", middleware.JWTProtected(), controllers.GetBlockInfo)                // get blockchain info
 
-	// Routes for PUT method:
-	route.Put("/book", middleware.JWTProtected(), controllers.UpdateBook) // update one book by ID
-
 	// Routes for DELETE method:
-	route.Delete("/book", middleware.JWTProtected(), controllers.DeleteBook) // delete one book by ID
 }
