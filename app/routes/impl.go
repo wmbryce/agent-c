54b660809d4924bbb9f7e5870413a5374d6094f9
Michael Bryce <michaelbryce@me.com>
2025-12-06 18:22:43 -0700

    fix: formatting

--- app/routes/impl.go
+++ app/routes/impl.go
@@ -19,7 +19,6 @@ func (r *Routes) Setup(app *fiber.App) {
 	v1.Get("/ai/models", r.service.GetModels)
 	v1.Post("/ai/models", r.service.CreateModel)
 	v1.Post("/ai/chat", service.ConsumeModel)
-
 	swaggerRoute := app.Group("/swagger")
 	swaggerRoute.Get("*", swagger.HandlerDefault)
 
